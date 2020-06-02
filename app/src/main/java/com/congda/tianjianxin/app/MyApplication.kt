9dbb5de409f9686e65cf300331ad44a154ed59fc
qiuqilin <951686695@qq.com>
2020-06-02 16:14:29 +0800

    save

--- app/src/main/java/com/congda/tianjianxin/app/MyApplication.kt
+++ app/src/main/java/com/congda/tianjianxin/app/MyApplication.kt
@@ -1,16 +1,33 @@
 package com.congda.tianjianxin.app
 
 import android.content.Context
+import cc.shinichi.library.tool.ui.ToastUtil
 import com.congda.baselibrary.app.BaseApplication
+import com.congda.tianjianxin.BuildConfig
+import com.congda.tianjianxin.app.Constant.BASE_URL
 
 class MyApplication : BaseApplication() {
 
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+        switchEnv()
     }
-
     companion object {
         var context: Context? = null
     }
+
+    private fun switchEnv() {
+        when(BuildConfig.BUILD_TYPE){
+            "debug"->{
+                BASE_URL=Constant.BASE_URL_DEBUG;
+                ToastUtil.getInstance()._short(this,"debug");
+            }
+            "release"->{
+                BASE_URL=Constant.BASE_URL_RELSESE;
+                ToastUtil.getInstance()._short(this,"release");
+            }
+        }
+    }
+
 }
